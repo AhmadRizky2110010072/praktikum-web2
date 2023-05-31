ba1a5a83795fafa03892b2ce8fec904cff48d938
AhmadRizky2110010072 <rizkylear@gmail.com>
2023-05-31 13:10:55 +0800

    commit pertama: menambahkan semua fail hasil prakitkum pertemuan 1 - 3

---
+++ profil.php
@@ -0,0 +1,50 @@
+<!-- <?php include "header.php" ?> -->
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/foto.jpg" alt="Ahmad Rizky" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>Npm :</strong></td>
+                        <td align="left" width="50%">2110010072</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Ahmad Rizky</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Nagara, 13 Mei 2002</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">082254401311</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Email :</strong></td>
+                        <td align="left" width="50%">rizkylear@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://instagram.com/a.rizzzky" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/instagram.svg" width="30" alt="instagram">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
