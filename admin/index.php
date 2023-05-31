355c562e2c6a7bcbcb32fbc967d053add712fd71
AhmadRizky2110010072 <rizkylear@gmail.com>
2023-05-31 15:19:49 +0800

    commit pertama: menambahkan semua fail hasil praktikum pertemuan 1 - 4

--- admin/index.php
+++ admin/index.php
@@ -10,7 +10,6 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- <base href="praktikum2020"> -->
     <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
     <title> Praktikum Web 2</title>
     <style>
@@ -30,7 +29,7 @@ session_start();
             background: #1fb359;
             padding: 10px 0;
             color: #fff;
-            font-family: Arial, Helvetica, sans-serif;
+            font-family: Arial, Helvetica, sans -serif;
             letter-spacing: 1.5px;
             text-align: center;
         }
@@ -44,46 +43,68 @@ session_start();
             <div class="col-md-3 col-sm-12 mb-4">
                 <?php include 'nav.php'; ?>
             </div>
+
             <div class="col-md-9 col-sm-12">
                 <?php include '../connection.php'; ?>
                 <?php
-                error_reporting(0);
-                switch ($_GET['page']) {
-                    default:
-                        include "dashboard.php";
-                        break;
-                    case "dashboard";
-                        include "dashboard.php";
-                        break;
-                        // mahasiswa 
-                    case "mahasiswa-show";
-                        include "../mahasiswa/mahasiswa_show.php";
-                        break;
-                    case "mahasiswa-add";
-                        include "../mahasiswa/mahasiswa_add.php";
-                        break;
-                    case "mahasiswa-edit";
-                        include "../mahasiswa/mahasiswa_edit.php";
-                        break;
-                    case "mahasiswa-delete";
-                        include "../mahasiswa/mahasiswa_delete.php.php";
-                        break;
-                    case "mahasiswa-update";
-                        include "../mahasiswa/mahasiswa_update.php";
-                        break;
-                        // user 
-                    case "user-add";
-                        include "../user/user_add.php";
-                        break;
-                    case "user-show";
-                        include "../user/user_show.php";
-                        break;
-                    case "user-edit";
-                        include "../user/user_edit.php";
-                        break;
-                    case "user-update";
-                        include "../user/user_update.php";
-                        break;
+                //error_reporting(0);
+                if (isset($_GET['page'])) {
+                    $page = $_GET['page'];
+                    switch ($_GET['page']) {
+                        case "dashboard";
+                            include "dashboard.php";
+                            break;
+                            // mahasiswa
+                        case "mahasiswa-show";
+                            include "../mahasiswa/mahasiswa_show.php";
+                            break;
+                        case "mahasiswa-add";
+                            include "../mahasiswa/mahasiswa_add.php";
+                            break;
+                        case "mahasiswa-edit";
+                            include "../mahasiswa/mahasiswa_edit.php";
+                            break;
+                        case "mahasiswa-delete";
+                            include "../mahasiswa/mahasiswa_delete.php.php";
+                            break;
+                        case "mahasiswa-update";
+                            include "../mahasiswa/mahasiswa_update.php";
+                            break;
+                            // user
+                        case "user-add";
+                            include "../user/user_add.php";
+                            break;
+                        case "user-show";
+                            include "../user/user_show.php";
+                            break;
+                        case "user-edit";
+                            include "../user/user_edit.php";
+                            break;
+                        case "user-update";
+                            include "../user/user_update.php";
+                            break;
+                        default:
+                            include "dashboard.php";
+                            break;
+                            //matakuliah
+                        case "matakuliah-show";
+                            include "../matakuliah/matakuliah_show.php";
+                            break;
+                        case "matakuliah-add";
+                            include "../matakuliah/matakuliah_add.php";
+                            break;
+                        case "matakuliah-edit";
+                            include "../matakuliah/matakuliah_edit.php";
+                            break;
+                        case "matakuliah-delete";
+                            include "../matakuliah/matakuliah_delete.php.php";
+                            break;
+                        case "matakuliah-update";
+                            include "../matakuliah/matakuliah_update.php";
+                            break;
+                    }
+                } else {
+                    include 'dashboard.php';
                 }
                 ?>
             </div>
